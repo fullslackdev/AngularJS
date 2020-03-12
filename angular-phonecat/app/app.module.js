5da58981151ba426c3f75fe85515d51bf1789611
Bart L <loeffen@hotmail.com>
2020-03-12 16:46:11 +0100

    First commit. Tutorial step 13.

---
+++ angular-phonecat/app/app.module.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Define the `phonecatApp` module
+angular.module('phonecatApp', [
+    // ...which depends on the `phoneList` module
+    'ngRoute',
+    'core',
+    'phoneDetail',
+    'phoneList'
+]);
