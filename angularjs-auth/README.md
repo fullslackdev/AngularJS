120d02fa80d7350d61e5d32bff554c6364cd64f9
Bart L <loeffen@hotmail.com>
2020-03-13 16:48:47 +0100

    Initial commit. Lesson 6 finished (setting up the project).

---
+++ angularjs-auth/README.md
@@ -0,0 +1,6 @@
+# AngularJS Authentication: Secure Your App with Auth0
+
+
+## Overview
+
+The full tutorial can be found at https://www.udemy.com/course/angularjs-authentication-with-auth0/
---
+++ angularjs-auth/app/navbar/navbar.directive.js
@@ -0,0 +1,15 @@
+(function() {
+    angular
+        .module('app')
+        .directive('navbar', navbar);
+
+    function navbar() {
+        return {
+            templateUrl: 'app/navbar/navbar.html',
+            controller: navbarController,
+            controllerAs: 'vm'
+        }
+    }
+
+    function navbarController() {}
+})();
